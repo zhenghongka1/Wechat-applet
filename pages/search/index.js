0b233ed2d09559902c99076ed23e0279a84a378c
郑鸿凯 <8694910+zheng-hongkai@user.noreply.gitee.com>
2021-11-02 17:14:02 +0800

    完成搜索功能开发

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,58 @@
 // pages/search/index.js
+import { request } from '../../request/request'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    goods: [],
+    // 取消按钮是否显示
+    isFocus: false,
+    // 文本框重置按钮
+    value: ''
+  },
+  // 防抖的定时器
+  Time: 1,
+  //1. 输入框输入值后进行改变就会触发事件
+  handleInput(e) {
+    // console.log(e);
+    const { value } = e.detail
+    // console.log(value);
+
+    // 2.检验合法性 trim为去除俩边的空格
+    if (!value.trim()) {
+      // 隐藏取消按钮
+      this.setData({
+        goods: [],
+        isFocus: false
+      })
+      // 当对搜索框进行删除的时候，可能会触发下面的定时器进而发送数据，所以在这里清空定时器
+      clearTimeout(this.Time)
+      // 值不合法
+      return
+    }
+    // 显示取消按钮
+    this.setData({
+      isFocus: true
+    })
+    // 3.准备发送请求获取数据（准备防抖技术）
+    clearTimeout(this.Time)
+    this.Time = setTimeout(() => {
+      this.qsearch(value)
+    }, 1000);
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 发送请求获取搜索建议的数据
+  async qsearch(query) {
+    const res = await request({ url: '/goods/qsearch', data: { query } })
+    const goods = res.data.message
+    // console.log(res);
+    this.setData({
+      goods
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleCancel() {
+    this.setData({
+      value: '',
+      isFocus: false,
+      goods: []
+    })
   }
 })
